289aa9eca3ffd9ce99cb83a25044aa27bc5f2e23
NikkiRufiansya <nikkirufiansya88@gmail.com>
2020-05-17 19:50:39 +0700

    update stelan

--- app/src/main/java/com/example/chatbot_stiki/Help.java
+++ app/src/main/java/com/example/chatbot_stiki/Help.java
@@ -2,15 +2,35 @@ package com.example.chatbot_stiki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Help extends AppCompatActivity {
-
+Button call, Email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help2);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        call = (Button) findViewById(R.id.phone);
+        Email = (Button) findViewById(R.id.email);
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String posted_by = "111-333-222-4";
+
+                String uri = "tel:" + posted_by.trim() ;
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+
     }
 }
